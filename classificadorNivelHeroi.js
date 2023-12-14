9be9f6318052581994411cd2f7acdc198d18ce29
BiaFreire <biafreire17@hotmail.com>
2023-12-14 14:56:29 -0300

    feat: initial files

---
+++ classificadorNivelHeroi.js
@@ -0,0 +1,20 @@
+let nomeHeroi =  "Mulher Maravilha"
+let quantidadeXp = 9700
+
+if (quantidadeXp < 1000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ferro");
+} else if (quantidadeXp >= 1001 && quantidadeXp <= 2000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Bronze");
+} else if (quantidadeXp >= 2001 && quantidadeXp <= 5000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Prata");
+} else if (quantidadeXp >= 6001 && quantidadeXp <= 7000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ouro");
+} else if (quantidadeXp >= 7001 && quantidadeXp <= 8000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Platina");
+} else if (quantidadeXp >= 8001 && quantidadeXp <= 9000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ascendente");
+} else if (quantidadeXp >= 9001 && quantidadeXp <= 10000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Imortal");
+} else if(quantidadeXp >= 10001) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Radiante");
+}
